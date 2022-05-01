f033151635b7a8397d3a2a4e8a4b67a70340228e
TheDonJinee <oma21007@byui.edu>
2022-05-01 04:54:36 +0100

    Week 2 assignment

---
+++ lesson2/js/script.js
@@ -0,0 +1,21 @@
+const thisDate = new Date(document.lastModified);
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const thisMonth = months[thisDate.getMonth()];
+const thisYear = thisDate.getFullYear();
+const currentDate = thisDate.getDate();
+const currentMonth = thisDate.getMonth() + 1;
+if ((thisDate.getHours >= 0 && thisDate.getSeconds === 0) && (thisDate.getHours <= 11 && thisDate.getSeconds <= 59)) {
+    isAm = 'pm'
+} else {
+    isAm = 'am'
+}
+let hh = thisDate.getHours();
+// if (hh < 10) {hh = '0'+ hh;}
+let mm = thisDate.getUTCMinutes();
+// if (mm < 10) {hh = '0'+ mm;}
+// const lastUpdated = `${thisMonth} ${currentDate}/${currentMonth}/${thisYear} ${hh}:${mm}${isAm}`;
+document.querySelector("#year").innerHTML = thisYear;
+document.querySelector("#datetime").innerHTML = thisDate;
+
+
+
