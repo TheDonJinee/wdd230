0e7e5b1de39f6c8bf9aa38e91e95aadf42f16868
TheDonJinee <oma21007@byui.edu>
2022-06-06 09:49:29 +0100

    Minor change

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -27,6 +27,39 @@ document.querySelector("#year").innerHTML = thisYear;
 document.querySelector("#datetime").innerHTML = thisDate;
 document.getElementById('headerDate').innerHTML = `${day}, ${date.getDate()} ${month} ${year}`;
 
+// Lazy Load
+// Get all the images with data-src attribute
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 200px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+
+imagesToLoad.forEach(image => {
+    imgObserver.observe(image);
+})
 
 // GET CURRENT DATE
 
